de73bddcd6d80721bb57f6588376d2296de16816
lhaof <lhaof@qq.com>
2017-04-01 15:31:09 +0800

    Update test_fcgan.lua

--- test_fcgan.lua
+++ test_fcgan.lua
@@ -131,35 +131,35 @@ for bat = 1, opt.batchNum do
     local refNetG_ctx = image_ctx:clone()
     local netG_ctx = image_ctx:clone()
     local ground_truth = image_ctx:clone()
-	-- [-1, 1]
+    -- [-1, 1]
 
-	-- save ground truth
-	local gt = {}
-	h = ground_truth:size(3); w = ground_truth:size(4);
-	for k,v in ipairs({56,64}) do
-		gt[v] = ground_truth[{{},{},{tp(h,v),bt(h,v)},{lf(w,v),rg(w,v)}}]:clone()
-	end
-	bcnt = bcnt + 1
+    -- save ground truth
+    local gt = {}
+    h = ground_truth:size(3); w = ground_truth:size(4);
+    for k,v in ipairs({56,64}) do
+        gt[v] = ground_truth[{{},{},{tp(h,v),bt(h,v)},{lf(w,v),rg(w,v)}}]:clone()
+    end
+    bcnt = bcnt + 1
 
-	-- do prediction
-	-- fill with imagenet20(fcgan)'s mean value
-	h = netG_ctx:size(3); w = netG_ctx:size(4);
-	netG_ctx[{{},{1},{tp(h,56),bt(h,56)},{lf(w,56),rg(w,56)}}]=mean_value1 * 2.0 - 1.0
-	netG_ctx[{{},{2},{tp(h,56),bt(h,56)},{lf(w,56),rg(w,56)}}]=mean_value2 * 2.0 - 1.0
-	netG_ctx[{{},{3},{tp(h,56),bt(h,56)},{lf(w,56),rg(w,56)}}]=mean_value3 * 2.0 - 1.0
-	netG_input:copy(netG_ctx)
+    -- do prediction
+    -- fill with imagenet20(fcgan)'s mean value
+    h = netG_ctx:size(3); w = netG_ctx:size(4);
+    netG_ctx[{{},{1},{tp(h,56),bt(h,56)},{lf(w,56),rg(w,56)}}]=mean_value1 * 2.0 - 1.0
+    netG_ctx[{{},{2},{tp(h,56),bt(h,56)},{lf(w,56),rg(w,56)}}]=mean_value2 * 2.0 - 1.0
+    netG_ctx[{{},{3},{tp(h,56),bt(h,56)},{lf(w,56),rg(w,56)}}]=mean_value3 * 2.0 - 1.0
+    netG_input:copy(netG_ctx)
     local netG_pred = netG:forward(netG_input):clone()
-	netG_pred = netG_pred[{{},{},{1 + opt.fineSize/4, opt.fineSize*3/4},{1 + opt.fineSize/4, opt.fineSize*3/4}}]:clone()
-	-- 128 x 128 -> 64 x 64
+    netG_pred = netG_pred[{{},{},{1 + opt.fineSize/4, opt.fineSize*3/4},{1 + opt.fineSize/4, opt.fineSize*3/4}}]:clone()
+    -- 128 x 128 -> 64 x 64
 
-	-- calculate loss
-	local netG_l1loss56
-	local netG_l2loss56
-	netG_l1loss56 = criterionABS:forward(crop_center(netG_pred,56),gt[56]:cuda())
-	netG_l2loss56 = criterionMSE:forward(crop_center(netG_pred,56),gt[56]:cuda())
+    -- calculate loss
+    local netG_l1loss56
+    local netG_l2loss56
+    netG_l1loss56 = criterionABS:forward(crop_center(netG_pred,56),gt[56]:cuda())
+    netG_l2loss56 = criterionMSE:forward(crop_center(netG_pred,56),gt[56]:cuda())
 
     netG_l1loss56_tot = netG_l1loss56_tot + netG_l1loss56
-	netG_l2loss56_tot = netG_l2loss56_tot + netG_l2loss56
+    netG_l2loss56_tot = netG_l2loss56_tot + netG_l2loss56
 
     if ( (opt.display_process == 1) and (bcnt % opt.disp_period == 0) ) then
         print('bat:'..bat..' bcnt:'..bcnt
@@ -168,44 +168,44 @@ for bat = 1, opt.batchNum do
     end
 
     -- paste predicted center in the context
-	h = netG_ctx:size(3); w = netG_ctx:size(4)
-	ph = netG_pred:size(3); pw = netG_pred:size(4)
-	netG_ctx[{{},{},{tp(h,56),bt(h,56)},{lf(w,56),rg(w,56)}}]:copy(
-		netG_pred[{{},{},{tp(ph,56),bt(ph,56)},{lf(pw,56),rg(pw,56)}}])
+    h = netG_ctx:size(3); w = netG_ctx:size(4)
+    ph = netG_pred:size(3); pw = netG_pred:size(4)
+    netG_ctx[{{},{},{tp(h,56),bt(h,56)},{lf(w,56),rg(w,56)}}]:copy(
+        netG_pred[{{},{},{tp(ph,56),bt(ph,56)},{lf(pw,56),rg(pw,56)}}])
 
     -- re-transform scale back to normal
     input_image_ctx:add(1):mul(0.5)
     image_ctx:add(1):mul(0.5)
-	netG_ctx:add(1):mul(0.5)
+    netG_ctx:add(1):mul(0.5)
 
-	netG_input:add(1):mul(0.5)
+    netG_input:add(1):mul(0.5)
 
-	netG_pred:add(1):mul(0.5)
+    netG_pred:add(1):mul(0.5)
 
-	ground_truth:add(1):mul(0.5)
-	gt[64]:add(1):mul(0.5)
-	gt[56]:add(1):mul(0.5)
+    ground_truth:add(1):mul(0.5)
+    gt[64]:add(1):mul(0.5)
+    gt[56]:add(1):mul(0.5)
 
     -- save outputs in a pretty manner
     gt[64]=nil; gt[56]=nil;
-	refNetG_pred=nil; netG_pred=nil;
+    refNetG_pred=nil; netG_pred=nil;
     local pretty_output = torch.Tensor(4*opt.batchSize, opt.nc, opt.fineSize, opt.fineSize)
 
-	h = refNetG_input:size(3); w = refNetG_input:size(4)
-	refNetG_input[{{},{},{tp(h,56),bt(h,56)},{lf(w,56),rg(w,56)}}] = 1
+    h = refNetG_input:size(3); w = refNetG_input:size(4)
+    refNetG_input[{{},{},{tp(h,56),bt(h,56)},{lf(w,56),rg(w,56)}}] = 1
 
     for i=1,opt.batchSize do
         pretty_output[4*i-3]:copy(refNetG_ctx[i])
         pretty_output[4*i-2]:copy(netG_ctx[i])
         pretty_output[4*i-1]:copy(refNetG_input[i])
-		pretty_output[4*i]:copy(ground_truth[i])
+        pretty_output[4*i]:copy(ground_truth[i])
     end
-	if (bcnt % opt.save_period == 0) then
-    	image.save(opt.name..'/'..opt.name..'_bcnt'..bcnt..'.png',image.toDisplayTensor(pretty_output))
-		if (opt.display_save == 1 ) then
-	    	print('Saved predictions to: ./'..opt.name..'_b'..bcnt..'.png')
-		end
-	end
+    if (bcnt % opt.save_period == 0) then
+        image.save(opt.name..'/'..opt.name..'_bcnt'..bcnt..'.png',image.toDisplayTensor(pretty_output))
+        if (opt.display_save == 1 ) then
+            print('Saved predictions to: ./'..opt.name..'_b'..bcnt..'.png')
+        end
+    end
 
 	::continue::
 end
